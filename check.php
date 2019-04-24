3940ac20b9979b9195a7ed779b70a984897b225c
kharee e owens <keo6@afsaccess1.njit.edu>
2019-04-23 20:36:53 -0400

    fixed checking process

---
+++ check.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+$pause = 5;
+
+
+if($_POST['captcha']!=$_SESSION['captcha'])
+	{
+		echo "Invalid captcha.<br>";
+		header ("refresh: $pause; url = index.php");
+		exit();
+	}
+ 
+if($_POST['captcha']==$_SESSION['captcha'])
+  {
+    echo "Correct captcha. Try another!<br>";
+    header("refresh: $pause; url = index.php");
+    exit();
+  }  
+  
+
+
+	
+?>
