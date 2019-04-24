25a06b8fbfb6257c544b24a52479aa9de6d745c2
kharee e owens <keo6@afsaccess1.njit.edu>
2019-04-23 20:44:52 -0400

    removing previous form input with autocomplete method

--- index.php
+++ index.php
@@ -10,16 +10,26 @@ session_start();
 		}
 	}
 ?>
-
+  <h3> Captcha Prompt!</h3>
+  <h4> Enter Captcha With a Space</h4>
 <style>
     .imgframe{
 	background: blue;
     padding: 10px;
     border: 5px dashed black; }
+    
+    
+    h1{
+      text-align: justified;
+    }
+    h4{
+      text-align: justified;
+    }
 </style>
 
-<form action = "check.php" method= "post">
+<form autocomplete="off" action = "check.php" method= "post">
 	<img src = "captcha.php" class = "imgframe" alt = "Captcha Image"/><br><br><br>
+
 	<input type = "text" name = "captcha"/>
 	<input type = "submit" value = "submit"/>
 </form>
