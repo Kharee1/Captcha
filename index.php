2c9115442be03801e7b63031844edcec9e3abd1d
kharee e owens <keo6@afsaccess1.njit.edu>
2019-04-23 20:24:46 -0400

    adding prompt to check if captcha works

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+	//print_r($_POST);
+	if(isset($_POST) & !empty($_POST)){
+		if($_POST['captcha']==$_SESSION['captcha']){
+			echo "Correct captcha";
+		}else{
+			echo "Incorrect captcha";
+		}
+	}
+?>
+
+<style>
+    .imgframe{
+	background: blue;
+    padding: 10px;
+    border: 5px dashed black; }
+</style>
+
+<form action = "" method= "post">
+	<img src = "captcha.php" class = "imgframe" alt = "Captcha Image"/><br><br><br>
+	<input type = "text" name = "captcha"/>
+	<input type = "submit" value = "submit"/>
+</form>
